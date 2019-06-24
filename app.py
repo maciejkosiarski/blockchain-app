155221bb4f748a31c9ae2173f96eb032578e5f61
Maciej Kosiarski <maciek.kosiarski@gmail.com>
2019-06-25 00:20:24 +0200

    verification class added

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from functools import reduce
 import json
-import pickle
-from dumper import dump
+# import pickle
+# from dumper import dump
 
-from hash_util import hash_string_256, hash_block
+from hash_util import hash_block
 from block import Block
 from transaction import Transaction
+from verification import Verification
 
 MINING_REWARD = 10
 
@@ -73,17 +74,12 @@ def save_data():
         print('Blockchain saving failed!')
 
 
-def valid_proof(transactions: list, last_hash: str, proof) -> bool:
-    guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
-    guess_hash = hash_string_256(guess)
-    return guess_hash[0:2] == '00'
-
-
 def proof_of_work() -> int:
     last_hash = hash_block(block_chain[-1])
     proof = 0
     print('Proof of work...')
-    while not valid_proof(open_transactions, last_hash, proof):
+    verifier = Verification()
+    while not verifier.valid_proof(open_transactions, last_hash, proof):
         proof += 1
 
     return proof
@@ -113,13 +109,10 @@ def get_last_block_chain_value():
     return block_chain[-1]
 
 
-def verify_transaction(transaction: Transaction) -> bool:
-    return get_balance(transaction.sender) >= transaction.amount
-
-
 def add_transaction(recipient: str, sender: str = owner, amount: float = 1.0):
+    verifier = Verification()
     transaction = Transaction(sender, recipient, amount)
-    if verify_transaction(transaction):
+    if verifier.verify_transaction(transaction, get_balance):
         open_transactions.append(transaction)
         save_data()
         return True
@@ -155,22 +148,6 @@ def print_blockchain_elements():
         print(20 * '-')
 
 
-def verify_chain():
-    for (index, block) in enumerate(block_chain):
-        if index == 0:
-            continue
-        if block.previous_hash != hash_block(block_chain[index - 1]):
-            return False
-        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-            print('Proof of work is invalid!')
-            return False
-    return True
-
-
-def verify_transactions():
-    return all([verify_transaction(tx) for tx in open_transactions])
-
-
 waiting_for_input = True
 
 while waiting_for_input:
@@ -198,7 +175,8 @@ while waiting_for_input:
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
-        if verify_transactions():
+        verifier = Verification()
+        if verifier.verify_transactions(open_transactions, get_balance):
             print('All transaction is valid!')
         else:
             print('There are invalid transactions')
@@ -206,7 +184,8 @@ while waiting_for_input:
         waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
-    if not verify_chain():
+    verifier = Verification()
+    if not verifier.verify_chain(block_chain):
         print_blockchain_elements()
         print('Invalid blockchain!')
         break
---
+++ verification.py
@@ -0,0 +1,26 @@
+from transaction import Transaction
+from hash_util import hash_string_256, hash_block
+
+
+class Verification:
+    def valid_proof(self, transactions: list, last_hash: str, proof) -> bool:
+        guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
+        guess_hash = hash_string_256(guess)
+        return guess_hash[0:2] == '00'
+
+    def verify_chain(self, block_chain):
+        for (index, block) in enumerate(block_chain):
+            if index == 0:
+                continue
+            if block.previous_hash != hash_block(block_chain[index - 1]):
+                return False
+            if not self.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+                print('Proof of work is invalid!')
+                return False
+        return True
+
+    def verify_transaction(self, transaction: Transaction, get_balance) -> bool:
+        return get_balance(transaction.sender) >= transaction.amount
+
+    def verify_transactions(self, open_transactions, get_balance):
+        return all([self.verify_transaction(tx, get_balance) for tx in open_transactions])
